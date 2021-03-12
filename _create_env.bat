49420f904d9226db80780586321a99b38ea6f905
w-vandijk-UT <59598904+w-vandijk@users.noreply.github.com>
2021-03-12 15:08:37 +0100

    Remove bugs

--- _create_env.bat
+++ _create_env.bat
@@ -3,21 +3,20 @@ REM Define the environname and installation path
 set env_name=conda-env
 set root=%~dp0
 set /p anaconda_dir=<%root%\requirements\anaconda_path.txt
-
-echo %anaconda_dir%
+echo Anaconda installation path: %anaconda_dir%
 
 REM Start anaconda prompt
 call %anaconda_dir%\scripts\activate.bat %anaconda_dir%
 
 REM Check if there is already an environment named %env_name%.
-REM If true: update packages from "environment.yml".
+REM If true: update packages f rom "environment.yml".
 IF exist %env_name% (
     ECHO The environment %env_name% already exists...
 	REM Check if environment.yml exists. If true: update packages from environment.yml.
-	IF exist environment.yml (
+	IF exist %root%\environment.yml (
 		ECHO Update packages from environment.yml...
 		call activate %root%\%env_name%
-		call conda env update -p %root%\%env_name% -f %root%environment.yml
+		call conda env update -p %root%\%env_name% -f %root%\environment.yml
 	REM else: do nothing
 	) else (
 		ECHO Nothing to update...
@@ -26,9 +25,9 @@ REM else: create an new environment
 ) else (
     ECHO %env_name% doesn't exist, creating a new environment...
 	REM Check if environment.yml exists. If true: create new environment with Environment.yml
-	IF exist requirements\environment.yml (
+	IF exist %root%\environment.yml (
 		ECHO Environment.yml exists, create environment with Environment.yml...
-		call conda env create --prefix %root%\%env_name% -f %root%environment.yml
+		call conda env create --prefix %root%\%env_name% -f %root%\environment.yml
 	REM else: create environment without packages
 	) else (
 		ECHO Environment.yml doesn't exist, create environment without Environment.yml...
@@ -50,3 +49,4 @@ Pause
 
 REM remove environment
 REM call conda remove -p %root%/%env_name% --all
+ 
